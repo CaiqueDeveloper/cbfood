19a0bfef69931b97c19fdbda01a809545c84255e
Caique Bispo <caiquebispo0023@gmail.com>
2022-06-01 19:40:35 -0300

    Ajuste name sapace heroku

--- app/Http/Controllers/Admin/Auth/AuthController.php
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
-use App\Http\Controllers\admin\UltilsController;
+use App\Http\Controllers\Admin\UltilsController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorageCompanyRequest;
 use App\Http\Requests\StorageUserRequester;
