3fc19a1983d46accbcd67dd3481c158969f52ec4
Caique Bispo <caiquebispo0023@gmail.com>
2022-06-15 18:42:52 -0300

    Correções notifiação pusher

--- public/js/app.js
+++ public/js/app.js
@@ -2111,19 +2111,24 @@ Pusher.logToConsole = true;
 var pusher = new Pusher(window.Laravel.pusher.key, {
   cluster: window.Laravel.pusher.cluster
 });
-navigator.serviceWorker.register('./sw.js');
 var channel = pusher.subscribe('new-order');
 channel.bind('App\\Events\\NotifyTheCompanySalesTheRequstUser', function (data) {
-  Notification.requestPermission(function (result) {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then(function (registration) {
-        registration.showNotification("Voc\xEA tem um novo pedido de ".concat(data.order.user[0].name), {
+  if (Notification.permission === 'grated') {
+    var notification = new Notification("Voc\xEA tem um novo pedido de ".concat(data.order.user[0].name), {
+      icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',
+      vibrate: window.navigator.vibrate([200])
+    });
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        var _notification = new Notification("Voc\xEA tem um novo pedido de ".concat(data.order.user[0].name), {
           icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200]
+          vibrate: window.navigator.vibrate([200])
         });
-      });
-    }
-  });
+      }
+    });
+  }
+
   Ultils.getNotifyComapy();
 });
 
