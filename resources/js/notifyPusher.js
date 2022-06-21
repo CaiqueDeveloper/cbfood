3c934b0fdf19007df854e1a32a3ba9a068e124ff
Caique Bispo <caiquebispo0023@gmail.com>
2022-06-21 10:33:36 -0300

    Criando arquivos base de ACL

--- resources/js/notifyPusher.js
+++ resources/js/notifyPusher.js
@@ -1,26 +1,25 @@
-
-
 Pusher.logToConsole = true;
 var pusher = new Pusher(window.Laravel.pusher.key, {
     cluster: window.Laravel.pusher.cluster
 });
 var channel = pusher.subscribe('new-order');
 channel.bind('App\\Events\\NotifyTheCompanySalesTheRequstUser', function(data) {
-    
-if(Notification.permission === 'granted'){
-    const notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
-        icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',
-        //vibrate: window.navigator.vibrate([200]),
-})
-}else if(Notification.permission !== "denied"){
-    Notification.requestPermission().then(permission =>{
-        if(permission === "granted"){
-            const notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
-            icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',
-            //vibrate: window.navigator.vibrate([200]),
-        })
-        }
-    })
-}
-Ultils.getNotifyComapy(); 
+    Ultils.getNotifyComapy();
+    if(!("Notification" in window)){
+
+        console.log("Esse navegador não suporta a notifiação no desktop");
+
+    }else if(Notification.permission === "granted"){
+
+        var notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
+            icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',});
+
+    }else if(Notification.permission !== 'denied' || Notification.permission === "default"){
+        Notification.requestPermission().then(function(permission) { 
+            if (permission === "granted") {
+                var notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
+                    icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',});
+            }
+        });
+    }
 });
