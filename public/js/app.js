bc01b14c8edd6151536baaf0d3b6e944e46f6eac
Caique Bispo <caiquebispo0023@gmail.com>
2022-07-17 16:47:34 -0300

    Rodando NPM RUN

--- public/js/app.js
+++ public/js/app.js
@@ -46,19 +46,22 @@ var pusher = new Pusher(window.Laravel.pusher.key, {
 var channel = pusher.subscribe('orders');
 channel.bind('sendOrderCompany', function (data) {
   Ultils.getNotifyComapy();
-  console.log(data.order); // if(!("Notification" in window)){
-  //     console.log("Esse navegador não suporta a notifiação no desktop");
-  // }else if(Notification.permission === "granted"){
-  //     var notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
-  //         icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',});
-  // }else if(Notification.permission !== 'denied' || Notification.permission === "default"){
-  //     Notification.requestPermission().then(function(permission) { 
-  //         if (permission === "granted") {
-  //             var notification = new Notification(`Você tem um novo pedido de ${data.order.user[0].name}`,{
-  //                 icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png',});
-  //         }
-  //     });
-  // }
+
+  if (!("Notification" in window)) {
+    console.log("Esse navegador não suporta a notifiação no desktop");
+  } else if (Notification.permission === "granted") {
+    var notification = new Notification("Voc\xEA tem um novo pedido de ".concat(data.order.user[0].name), {
+      icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png'
+    });
+  } else if (Notification.permission !== 'denied' || Notification.permission === "default") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Voc\xEA tem um novo pedido de ".concat(data.order.user[0].name), {
+          icon: 'https://i.postimg.cc/W3Mhx5s3/Group-1-2.png'
+        });
+      }
+    });
+  }
 });
 
 /***/ }),
