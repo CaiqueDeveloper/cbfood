6d3de23cb9396920a98683701cbdb1ba25ff4297
Caique Bispo <caiquebispo0023@gmail.com>
2022-07-27 10:42:50 -0300

    Teste envio de notificaçõe FCM

--- app/Http/Controllers/Admin/SendNotificationFCMController.php
+++ app/Http/Controllers/Admin/SendNotificationFCMController.php
@@ -3,48 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SendNotificationFCMController extends Controller
 {
-    public function sendPushNotification($id = null) {  
-			$fcm_token = 'AAAArd5hIIQ:APA91bF1sooeQMnf-ig1Cqp-iRpfCGCyJZWB0EfWoyuNYS-eQL7olwX1qQm0LQMvD4_XCA5VaMQOcS8nkk1dGtsbhFr9MVuoKzlQwH-VHKv_D4lIaPKfm0jZu_Qw_VbGiEXaW1mPiMsI';
-			$title = 'TESTE';
-			$message = 'OLÁ';
-
-			$url = "https://fcm.googleapis.com/fcm/send";            
-			$header = [
-			'authorization: key='.$fcm_token,
-				'content-type: application/json'
-			];    
-	
-			$postdata = '{
-				"to" : "' . $fcm_token . '",
-					"notification" : {
-						"title":"' . $title . '",
-						"text" : "' . $message . '"
-					},
-				"data" : {
-					"id" : "'.$id.'",
-					"title":"' . $title . '",
-					"description" : "' . $message . '",
-					"text" : "' . $message . '",
-					"is_read": 0
-				  }
-			}';
-	
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-	
-			$result = curl_exec($ch);    
-			curl_close($ch);
-	
-			return $result;
-		}
+    public function sendNotification(Request $request)
+    {
+        $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+            
+        $SERVER_API_KEY = env('FCM_SERVER_KEY');
+    
+        $data = [
+            "registration_ids" => $firebaseToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,  
+            ]
+        ];
+        $dataString = json_encode($data);
+      
+        $headers = [
+            'Authorization: key=' . $SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+      
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+                 
+        $response = curl_exec($ch);
+		dd($response);
+    }
         
 }
