19a2bc26eb17574cd02b7ab852fb54eed22f1556
Caique Bispo <caiquebispo0023@gmail.com>
2022-07-31 15:38:13 -0300

    Migrando os campos de associacao da tabela user para tabela compaies

--- database/migrations/2019_05_03_000001_create_customer_columns.php
+++ database/migrations/2019_05_03_000001_create_customer_columns.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('companies', function (Blueprint $table) {
             $table->string('stripe_id')->nullable()->index();
             $table->string('pm_type')->nullable();
             $table->string('pm_last_four', 4)->nullable();
