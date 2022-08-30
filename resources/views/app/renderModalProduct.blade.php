6ea1003fbd25fa7d5c5bf1ff008ec186716d23e4
Caique Bispo <caiquebispo0023@gmail.com>
2022-08-30 10:15:35 -0300

    Adicionando configuração de cores dinamica na exibição do modal de produto

--- resources/views/app/renderModalProduct.blade.php
+++ resources/views/app/renderModalProduct.blade.php
@@ -6,13 +6,12 @@
             <div class="contente-product-imgs-controls w-full flex justify-between hidden">
                 <div class="prev w-[50px] h-[50px] bg-white  text-orange-600 rounded-full text-center shadow-lg  cursor-pointer ml-3 leading-[45px] font-bold text-2xl"><</div>
                 <div class="next w-[50px] h-[50px] bg-white  text-orange-600 rounded-full text-center shadow-lg  cursor-pointer mr-3 leading-[45px] font-bold text-2xl">></div>
-
             </div>
         </div>
         <div class="content-product-name mr-3 ml-3 my-3">
             <h1 class="text-xl sm:text-4xl font-bold text-black-600 product-name">{{$product['product']->name}}</h1>
             <div class="content-produc-category flex mt-3">
-                <p class="text-sm text-black-600 font-bold" >Categoria:</p> <p class="text-sm  font-bold  bg-orange-300 text-orange-600 ml-2 rounded-lg px-1">{{$product['product']->category->name}}</p>
+                <p class="text-sm text-black-600 font-bold" >Categoria:</p> <p class="text-sm  font-bold  @if($company['company']->settings[0]->secondColor != null) bg-[{{$company['company']->settings[0]->secondColor}}] @else  bg-orange-300 @endif @if($company['company']->settings[0]->primaryColor != null) text-[{{$company['company']->settings[0]->primaryColor}}] @else  text-orange-600 @endif ml-2 rounded-lg px-1">{{$product['product']->category->name}}</p>
 
             </div>
         </div>
@@ -35,7 +34,13 @@
                 <h1 class="text-xl sm:text-2xl font-bold text-black-600 mr-3 ml-3">Selecione o tamanho desejado</h1>
                 <div class="product-content-info-sizes flex flex-wrap sm:flex-nowrap my-3">
                     @foreach ($product['product']->variations as $key => $value)
-                        <div data-key="{{$key}}" data-price_variation_product="{{$value['variationPrice']}}" data-variation_id="{{$value['id']}}"class="product-content-info-size mb-2 col col-sm-2 @if($maxValue['variationPrice'] == $value['variationPrice'])  bg-orange-300  @endif border-2 border-orange-300  font-medium  hover:bg-orange-300 px-[10px] ml-2 py-[15px] text-orange-600  hover:text-white cursor-pointer text-sm rounded-lg font-extrabold">{{$value['variationName']}}<span class="ml-2  text-orange-600  text-xs">{{$value['variationType']}}</span></div>   
+                        <div data-key="{{$key}}" data-price_variation_product="{{$value['variationPrice']}}" data-bgColorItemSelect="@if($company['company']->settings[0]->secondColor != null) bg-[{{$company['company']->settings[0]->secondColor}}] @else  bg-orange-300 @endif"
+                        data-variation_id="{{$value['id']}}"class="product-content-info-size mb-2 col col-sm-2 
+                        @if($maxValue['variationPrice'] == $value['variationPrice']) @if($company['company']->settings[0]->secondColor != null) bg-[{{$company['company']->settings[0]->secondColor}}] @else  bg-orange-300 @endif  @endif 
+                        border-2 @if($company['company']->settings[0]->primaryColor != null) text-[{{$company['company']->settings[0]->primaryColor}}] @else  border-orange-300 @endif   font-medium  @if($company['company']->settings[0]->secondColor != null)  hover:bg-[{{$company['company']->settings[0]->secondColor}}] @else  hover:bg-orange-300 @endif  
+                        px-[10px] ml-2 py-[15px] @if($company['company']->settings[0]->primaryColor != null) border-[{{$company['company']->settings[0]->secondColor}}] @else  text-orange-600 @endif  hover:text-white cursor-pointer 
+                        text-sm rounded-lg font-extrabold">
+                        {{$value['variationName']}}<span class="ml-2  @if($company['company']->settings[0]->primaryColor != null) text-[{{$company['company']->settings[0]->primaryColor}}] @else  text-orange-600 @endif  text-xs">{{$value['variationType']}}</span></div>   
 
                     @endforeach 
                 </div>
@@ -95,9 +100,9 @@
                 
                 @endif
                 <div class="product-content-info--qtarea flex items-center  h-[30px] rounded-[10px] px-[10px]">
-                    <button class="product-content-info--qtmenos  px-[10px] text-lg  bg-orange-600 text-white font-bold rounded-lg">-</button>
+                    <button class="product-content-info--qtmenos  px-[10px] text-lg  @if($company['company']->settings[0]->secondColor != null) bg-[{{$company['company']->settings[0]->secondColor}}] @else  bg-orange-300 @endif @if($company['company']->settings[0]->primaryColor != null) text-[{{$company['company']->settings[0]->primaryColor}}] @else  text-orange-600 @endif font-bold rounded-lg">-</button>
                     <div class="product-content-info--qt mx-3 font-bold">1</div>
-                    <button class="product-content-info--qtmais px-[10px] text-lg  bg-orange-600 text-white font-bold rounded-lg">+</button>
+                    <button class="product-content-info--qtmais px-[10px] text-lg  @if($company['company']->settings[0]->secondColor != null) bg-[{{$company['company']->settings[0]->secondColor}}] @else  bg-orange-300 @endif @if($company['company']->settings[0]->primaryColor != null) text-[{{$company['company']->settings[0]->primaryColor}}] @else  text-orange-600 @endif font-bold rounded-lg">+</button>
 
                 </div>
             </div>
