d4fab1814ee574796274a015e799651d89099d77
Caique Bispo <caiquebispo0023@gmail.com>
2022-09-04 15:57:47 -0300

    Criando travas para não possibilitar a realização de venda em empresa desativada

--- resources/views/app/modals/modalCartItem.blade.php
+++ resources/views/app/modals/modalCartItem.blade.php
@@ -41,11 +41,13 @@
         
     </div>
     <div class="checout sm:mr-3 sm:ml-3 my-4 flex items-center">
+        @if($company['company']['status'])
         <div class="add-cart bg-green-300 text-green-600 font-bold p-2 rounded-xl mr-4 cursor-pointer flex items-center">Proximo 
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill ml-2" viewBox="0 0 16 16">
                 <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
             </svg>
         </div>
+        @endif
         <div class="closed-modal bg-red-300 text-red-600 font-bold p-2 rounded-xl cursor-pointer clear-cart flex items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash text-red" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
