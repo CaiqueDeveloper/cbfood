07e47ee4020d3df4c10ee2ba7fc09036eebba093
Caique Bispo <caiquebispo0023@gmail.com>
2022-09-08 10:29:25 -0300

    subindo funcionalidade de slide show

--- resources/views/app/renderModalProduct.blade.php
+++ resources/views/app/renderModalProduct.blade.php
@@ -1,13 +1,35 @@
 
 <div class="announcementModalArea  w-full h-screen fixed top-0 left-0 bg-[#33333387] z-[100]  overflow-y-auto"  data-company_id="{{$product['product']->product_morph_id}}">
-    <div class="content-product-item bg-white w-full sm:max-w-[750px] rounded-lg mx-auto pb-4 sm:mt-[75px]">
-        <div class="content-product-imgs relative overflow-hidden">
-            <div class="img-product w-full min-h-[300px] mb-4  bg-cover bg-no-repeat bg-center flex items-center rounded-t-lg" style="background-image: url('/product_photo/@if(count($product['product']->images) > 0){{$product['product']->images->last()->path}}@else/default/default.jpg @endif')">
-            <div class="contente-product-imgs-controls w-[110px]  bg-gradient-to-b from-white h-[300px] drop-shadow-md  flex flex-col items-center overflow-x-auto hidden">
+    <div class="content-product-item bg-white w-full sm:max-w-[750px] rounded-lg mx-auto pb-4 sm:mt-[75px] max-h-screen">
+        <div class="relative">
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg sm:h-64 xl:h-80 2xl:h-96 ">
                 @foreach($product['product']->images as $key => $value)
-                    <img src="/product_photo/{{$value->path}}" alt=""width="100" height="100" class="my-2">
+                <div id="carousel-item-{{$key}}" class="duration-700 ease-in-out content-slide">
+                    <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
+                    <img src="/product_photo/{{$value->path}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                </div>
                 @endforeach
             </div>
+            <!-- Slider indicators -->
+            <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                @foreach($product['product']->images as $key => $value)
+                <button id="carousel-indicator-{{$key}}" type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"></button>
+                @endforeach
+            </div>
+            <!-- Slider controls -->
+            <button id="data-carousel-prev" type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                    <span class="hidden">Previous</span>
+                </span>
+            </button>
+            <button id="data-carousel-next" type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span class="hidden">Next</span>
+                </span>
+            </button>
         </div>
         <div class="content-product-name mr-3 ml-3 my-3">
             <h1 class="text-xl sm:text-4xl font-bold text-black-600 product-name">{{$product['product']->name}}</h1>
@@ -75,8 +97,7 @@
                         </div>
                     </div>
                     @empty
-                    @endforelse
-                    
+                    @endforelse   
                 </div>
             </div>
         @endif
@@ -138,12 +159,47 @@
                     </div>
                 </div>
             </div>
+            <div class="product-content-add-cart mr-3 ml-3 my-4 flex items-center ">
+                @if($company['company']['status'])
+                    <div class="add-cart bg-green-300 text-green-600 font-bold p-2 rounded-xl mr-4 cursor-pointer" data-product_id="{{$product['product']->id}}">Adicionar ao Carrinho</div>
+                @endif
+                <div class="closed-modal bg-red-300 text-red-600 font-bold p-2 rounded-xl cursor-pointer">Cancelar</div>
+            </div>
         </div>
-        <div class="product-content-add-cart mr-3 ml-3 my-4 flex items-center">
-            @if($company['company']['status'])
-                <div class="add-cart bg-green-300 text-green-600 font-bold p-2 rounded-xl mr-4 cursor-pointer" data-product_id="{{$product['product']->id}}">Adicionar ao Carrinho</div>
-            @endif
-            <div class="closed-modal bg-red-300 text-red-600 font-bold p-2 rounded-xl cursor-pointer">Cancelar</div>
-        </div>
+        
     </div>
 </div>
+<script scoped>
+    const items  = [];
+    const options = {
+        activeItemPosition: 0,
+        interval: 3000,
+        indicators: {
+            activeClasses: 'bg-white dark:bg-gray-800',
+            inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+            items: []
+        }
+    };
+    $(".content-slide" ).each(function( index ) {
+        items.push({
+            position: index,
+            el: document.getElementById('carousel-item-'+index)
+        })
+        options.indicators.items.push({
+            position: index,
+            el: document.getElementById('carousel-indicator-'+index)
+        })
+    });
+
+    const carousel = new Carousel(items, options);
+    const prevButton = document.getElementById('data-carousel-prev');
+    const nextButton = document.getElementById('data-carousel-next');
+
+    prevButton.addEventListener('click', () => {
+        carousel.prev();
+    });
+
+    nextButton.addEventListener('click', () => {
+        carousel.next();
+    });
+</script>
