cba1978dca50f26066509620e4d00f08ed7d9a9e
Caique Bispo <caiquebispo0023@gmail.com>
2022-09-08 09:13:12 -0300

    Finalizando tela do cliete

--- resources/views/layouts/include/app/head.blade.php
+++ resources/views/layouts/include/app/head.blade.php
@@ -23,7 +23,7 @@
     <title>Cbfood - @yield('title')</title>
 
     {{-- tailwindcss --}}
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     {{-- <script src="{{asset('js/tailwindcss.js')}}"></script> --}}
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <!-- Custom fonts for this template-->
@@ -38,6 +38,6 @@
     <link rel="icon" href="{{ url('panel/img/logo/icon-page.svg') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <script src="https://js.pusher.com/7.1/pusher.min.js"></script>
-    <link href="/css/app.css" rel="stylesheet">
+    {{-- <link href="/css/app.css" rel="stylesheet"> --}}
     <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
 </head>
